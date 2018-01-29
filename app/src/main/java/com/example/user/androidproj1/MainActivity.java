e36a42147013dadd97c7777f13065739413730ea
Banana Android Z <ndrdbnnz@gmail.com>
2018-01-29 19:07:46 +0300

    обработка кодом

--- app/src/main/java/com/example/user/androidproj1/MainActivity.java
+++ app/src/main/java/com/example/user/androidproj1/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.user.androidproj1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    EditText userName;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+    private void initialize() {
+        //находим тектовое поля кнопку
+        userName = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2){
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                check();
+            }
+        });
+    }
+    private void check() {
+        if (TextUtils.isEmpty(userName.getText())) {
+            loginButton.setEnabled(false);
+        } else {
+            loginButton.setEnabled(true);
+        }
     }
 }
